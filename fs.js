6b9fa67b10b3e0d80167359f5e7bc6ad243cad9e
sherif altiby <sherifahmed852002@gmail.com>
2024-06-30 23:42:04 +0300

    File sysytem in node js

---
+++ fs.js
@@ -0,0 +1,41 @@
+const fs = require('node:fs');
+
+//  Read File
+const fileContent  = fs.readFileSync('./hellow.txt', 'utf8')
+console.log("fileContent: ", fileContent)
+
+
+fs.readFile('./users.json', 'utf8', (err, data) => {
+    if(err){
+        console.log("Error Of Reading File: ", err)
+    } else {
+        console.log("File Data: ", data)
+    }
+})
+
+
+//  Write File
+
+const data =[
+    {
+        id: 1,
+        name: "Sherif"
+    }
+]
+
+fs.writeFile('./users.json', JSON.stringify(data), 'utf8', (err) => {
+    if(err){
+        console.log('Error From File: ', err)
+    } else {
+        console.log("File Added Done")
+    }
+})
+
+// Delete File 
+fs.unlink('./users.json', (err) => {
+    if(err){
+        console.log("There is an error")
+    } else {
+        console.log("File removd successfully!")
+    }
+})
